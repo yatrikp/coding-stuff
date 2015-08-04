423ed40d82b8f32faab9910c869c13c410585cf9
Yatrik Mehta <yatrikp@gmail.com>
2015-08-03 19:28:14 -0700

    initial commit

---
+++ NumbersToWords.java
@@ -0,0 +1,111 @@
+package com.test.strings;
+
+/**
+ * Created by meera on 8/3/15.
+ */
+public class NumbersToWords {
+
+    private static final String[] specialNames = {
+            "",
+            " thousand",
+            " million",
+            " billion",
+            " trillion",
+            " quadrillion",
+            " quintillion"
+    };
+
+    private static final String[] tensNames = {
+            "",
+            " ten",
+            " twenty",
+            " thirty",
+            " forty",
+            " fifty",
+            " sixty",
+            " seventy",
+            " eighty",
+            " ninety"
+    };
+
+    private static final String[] numNames = {
+            "",
+            " one",
+            " two",
+            " three",
+            " four",
+            " five",
+            " six",
+            " seven",
+            " eight",
+            " nine",
+            " ten",
+            " eleven",
+            " twelve",
+            " thirteen",
+            " fourteen",
+            " fifteen",
+            " sixteen",
+            " seventeen",
+            " eighteen",
+            " nineteen"
+    };
+
+    private static String convertLessThanOneThousand(int number) {
+        String current;
+
+        if (number % 100 < 20){
+            current = numNames[number % 100];
+            number /= 100;
+        }
+        else {
+            current = numNames[number % 10];
+            number /= 10;
+
+            current = tensNames[number % 10] + current;
+            number /= 10;
+        }
+        if (number == 0) return current;
+        return numNames[number] + " hundred" + current;
+    }
+
+    public static String convert(int number) {
+
+        if (number == 0) {
+            return "zero";
+        }
+
+        String prefix = "";
+
+        if (number < 0) {
+            number = -number;
+            prefix = "negative";
+        }
+
+        String current = "";
+        int place = 0;
+
+        while (number > 0){
+            int n = number % 1000;
+            if (n != 0){
+                String s = convertLessThanOneThousand(n);
+                current = s + specialNames[place] + current;
+            }
+            place++;
+            number /= 1000;
+        }
+
+        return (prefix + current).trim();
+    }
+
+    public static void main(String[] args) {
+        String phrase = "12345.67" ;
+        Float num = new Float(phrase) ;
+        int dollars = num.intValue();
+        int cent = Math.round((num - dollars) * 100);
+
+        String s = NumbersToWords.convert(dollars) + " and "
+                +  cent + "/100 dollars" ;
+        System.out.println(s);
+    }
+}
